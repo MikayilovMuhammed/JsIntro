6a1b5a07310f8ef90125db14d99c22227c625cc1
MikayilovMuhammed <92036426+MikayilovMuhammed@users.noreply.github.com>
2021-12-07 01:37:04 +0400

    Javascript intro

---
+++ js/index.js
@@ -0,0 +1,86 @@
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+// Object
+
+// var test1 = 5.5;
+// var test2 = false;
+
+// var und; // Undefined = undefined
+// var null1 = null;
+
+// // console.log(test);
+
+// // console.log("Salam ", test1);
+
+// // console.log(typeof test);
+// // console.log(typeof test1);
+// // console.log(typeof test2);
+// // console.log(typeof null1);
+// // console.log(typeof und);
+
+// var person = {
+//   name: "Muhammed",
+//   surname: "Mikayilov",
+//   status: false,
+// };
+
+// var arr = [];
+
+// console.log(test);
+// console.log(val1);
+
+// Hoisting
+var test;
+
+// function Sum(a, b) {
+//   return a + b;
+// }
+
+// test = "Hello";
+
+// if (val1 === undefined) {
+// }
+
+console.log("Test", test);
+var test = "Hello";
+let val1 = "Hello";
+const test2 = "Hello";
+
+test = 5;
+val1 = 4;
+// test2 = 15;
+
+// console.log("Let: ", val1);
+console.log("Var: ", hjdsfghjgj);
+var hjdsfghjgj = "dfhbjghdsbhjf";
+// console.log("Var: ", test2);
+
+const obj = {
+  name: "Elnur",
+  surname: "Maharramli",
+  isMarried: false,
+};
+
+console.log(Sum(5, 19));
+
+function Sum(a, b) {
+  return a + b;
+}
+
+// console.log(obj);
+
+// obj.age = 28;
+
+// obj.isMarried = true;
+
+// console.log(obj.age);
+// console.log(obj);
+
+// const arr = ["Salam", 10, false];
+// console.log(arr);
+
+// let a = 50;
+// console.log(Sum(5, 15));
